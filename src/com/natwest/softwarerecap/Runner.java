77eef3c2b189f2eb1c94f0f0a48b9d797bf30c40
Kersji <kersji@gmail.com>
2021-08-02 10:58:40 +0100

    Java recap

---
+++ src/com/natwest/softwarerecap/Runner.java
@@ -0,0 +1,14 @@
+package com.natwest.softwarerecap;
+
+public class Runner {
+
+	public static void main(String [] Args) {
+		
+		printName("Kersji");
+		
+	}
+	
+	public static void printName(String name) {
+		System.out.println("Hello " + name);
+	}
+}
